55a473ea23eda1fb356316b7d83cab3d991d34eb
Midhun <midhunskani@gmail.com>
2025-10-14 22:41:37 +0530

    updated sections

--- src/skills.jsx
+++ src/skills.jsx
@@ -41,7 +41,7 @@ function Skills() {
                     </div>
                     <div className="flex items-center gap-3 rounded-lg border  p-4">
                         <img width="24" height="24" src="https://img.icons8.com/color/48/bootstrap--v2.png" alt="bootstrap--v2"/>
-                        <h2 className="text-base leading-tight font-bold">Tailwind CSS</h2>
+                        <h2 className="text-base leading-tight font-bold">Bootstrap</h2>
                     </div>
 
                     <div className="flex items-center gap-3 rounded-lg border  p-4">
@@ -60,13 +60,64 @@ function Skills() {
                         <h2 className="text-base leading-tight font-bold">Python</h2>
                     </div>
                     <div className="flex items-center gap-3 rounded-lg border  p-4">
-                        <img width="24" height="24" src="https://img.icons8.com/color/48/django.png" alt="django"/>
+                        <img width="24" height="24"
+                             src="https://img.icons8.com/external-tal-revivo-filled-tal-revivo/24/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-filled-tal-revivo.png"
+                             alt="django"/>
                         <h2 className="text-base leading-tight font-bold">Django</h2>
                     </div>
                     <div className="flex items-center gap-3 rounded-lg border  p-4">
-                        <img width="24" height="24" src="https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png" alt="django"/>
-                        {/*<h2 className="text-base leading-tight font-bold">Django</h2>*/}
+                        <img width="24" height="24" src="https://tinyurl.com/55b369pd" alt="django"/>
+                        <h2 className="text-base leading-tight font-bold">Django Ninja</h2>
                     </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24"
+                             src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo.png"
+                             alt="external-postgre-sql-a-free-and-open-source-relational-database-management-system-logo-color-tal-revivo"/>
+                        <h2 className="text-base leading-tight font-bold">Postgres SQL</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://img.icons8.com/color/48/pandas.png" alt="pandas"/>
+                        <h2 className="text-base leading-tight font-bold">Pandas</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24"
+                             src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/external-amazon-web-services-a-subsidiary-of-amazon-that-provides-on-demand-cloud-computing-logo-shadow-tal-revivo.png"
+                             alt="external-amazon-web-services-a-subsidiary-of-amazon-that-provides-on-demand-cloud-computing-logo-shadow-tal-revivo"/>
+                        <h2 className="text-base leading-tight font-bold">AWS</h2>
+                    </div>
+                </div>
+                <h2 className="text-2xl font-bold">Other Tools</h2>
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 py-4">
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://img.icons8.com/color/48/cloud-function.png" alt="cloud-function"/>
+                        <h2 className="text-base leading-tight font-bold">Swagger</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://img.icons8.com/dusk/64/postman-api.png" alt="postman-api"/>
+                        <h2 className="text-base leading-tight font-bold">Postman</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://raw.githubusercontent.com/usebruno/bruno/main/assets/images/logo-transparent.png" alt="postman-api"/>
+                        <h2 className="text-base leading-tight font-bold">Bruno</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://img.icons8.com/color/48/figma--v1.png" alt="figma--v1"/>
+                        <h2 className="text-base leading-tight font-bold">Figma</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/external-docker-a-set-of-coupled-software-as-a-service-logo-color-tal-revivo.png" alt="external-docker-a-set-of-coupled-software-as-a-service-logo-color-tal-revivo"/>
+                        <h2 className="text-base leading-tight font-bold">Docker</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://avatars.githubusercontent.com/u/10520629?s=200&v=4" alt="external-docker-a-set-of-coupled-software-as-a-service-logo-color-tal-revivo"/>
+                        <h2 className="text-base leading-tight font-bold">Metabase</h2>
+                    </div>
+                    <div className="flex items-center gap-3 rounded-lg border  p-4">
+                        <img width="24" height="24" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/external-github-community-for-software-building-and-testing-online-logo-shadow-tal-revivo.png" alt="external-github-community-for-software-building-and-testing-online-logo-shadow-tal-revivo"/>
+                        <h2 className="text-base leading-tight font-bold">Github</h2>
+                    </div>
+
+
                 </div>
             </div>
 
